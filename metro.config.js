cca9a9f8daa8d076193dd0db2e118e6c7e3b421a
suhailkt <suhaiktmit@gmail.com>
2026-07-24 01:23:30 +0530

    Upgrade Node to 20 and polyfill Array.prototype.toReversed in metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,10 @@
+// Polyfill Array.prototype.toReversed for compatibility with Expo SDK 54 / Metro 0.81
+if (!Array.prototype.toReversed) {
+  Array.prototype.toReversed = function() {
+    return this.slice().reverse();
+  };
+}
+
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
